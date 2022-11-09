1bdc55f68ac0df37b676d36608778de4b15389f2
Erick Bacelar Rodrigues <erickrodrigues0150@gmail.com>
2022-11-09 11:37:45 -0300

    Setup do projeto e dependencias inicias adicionadas

---
+++ src/main/java/br/ebr/autores/ApiAutoresApplication.java
@@ -0,0 +1,13 @@
+package br.ebr.autores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAutoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAutoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ebr/autores/ApiAutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ebr.autores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
